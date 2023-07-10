6328bd6d07750b3b48af9a6400afa2861276bc17
cyjin463 <cyjin463@hanmail.net>
2023-07-10 22:39:05 +0900

    20230710disneyModal

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import axios from "../api/axios";
 import React, { useCallback, useEffect, useState } from "react";
 import "./Row.css";
+import MovieModal from "./MovieModal";
 
 const Row = ({ title, id, fetchUrl }) => {
     const [movies, setMovies] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [movieSelected, setMovieSelection] = useState({});
 
     const fetchMovieData = useCallback(async () => {
         const response = await axios.get(fetchUrl);
@@ -13,6 +16,11 @@ const Row = ({ title, id, fetchUrl }) => {
     useEffect(() => {
         fetchMovieData();
     }, [fetchMovieData]);
+
+    const handleClick = (movie) => {
+        setModalOpen(true);
+        setMovieSelection(movie);
+    };
 
     return (
         <div>
@@ -34,6 +42,7 @@ const Row = ({ title, id, fetchUrl }) => {
                             className="row__poster"
                             src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
                             alt={movie.name}
+                            onClick={() => handleClick(movie)}
                         />
                     ))}
                 </div>
@@ -47,6 +56,8 @@ const Row = ({ title, id, fetchUrl }) => {
                     </span>
                 </div>
             </div>
+
+            {modalOpen && <MovieModal setModalOpen={setModalOpen} {...movieSelected} />}
         </div>
     );
 };
